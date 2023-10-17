0fb1ca96f556dcbfa8d135cda5d52aa2eb086aab
SATPAL-20 <131527626+SATPAL-20@users.noreply.github.com>
2023-10-17 20:32:49 +0530

    main.py

--- main.py
+++ main.py
@@ -2,33 +2,24 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 # Model and tokenizer names
-adapters_name = "1littlecoder/mistral-7b-mj-finetuned"
-model_name = "bn22/Mistral-7B-Instruct-v0.1-sharded"
+model_name = "bn22/Mistral-7B-Instruct-v0.1-sharded"  # Update with the correct model name
+tokenizer_name = "bn22/Mistral-7B-Instruct-v0.1-sharded"  # Update with the correct tokenizer name
 
-# Device
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print("Using device:", device)
-
-# Load the tokenizer
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-
-# Load the model
+# Load the tokenizer and model
+tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
 model = AutoModelForCausalLM.from_pretrained(model_name)
-model = model.to(device)
-
-print(f"Successfully loaded the model {model_name} into memory")
 
 # Prompt the user to enter the input text
 user_input = input("Enter your input: ")
 
 # Replace [INST] and [/INST] with user's input
-input_text = f"{user_input}"
+input_text = f"[INST]{user_input}[/INST]"
 
 # Encode the input text
 encoded = tokenizer(input_text, return_tensors="pt", add_special_tokens=False)
-model_input = encoded.to(device)
 
 # Generate output using the encoded input
-generated_ids = model.generate(**model_input, max_length=200, do_sample=True)
-decoded = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
-print("Generated output:", decoded)
+generated_ids = model.generate(encoded["input_ids"], max_length=200, num_return_sequences=1)
+decoded = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
+
+print(decoded[0])
